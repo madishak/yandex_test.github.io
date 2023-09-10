0b7008e405238c47fcee7640218adc81a8dd8da8
Madina <medinakarachai@mail.ru>
2023-09-10 14:40:44 +0300

    Added files

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,28 @@
-import { Link } from 'react-router-dom';
+import './styles.css';
+import { Link } from "react-router-dom";
 
 const About = () => {
-    return (
-        <div>
-            <h1>Madina Karachai</h1>
-            <img src='' />
-            <a href="">Git репозиторий</a>
-            <p></p>           
-            <Link to='game'>Play Game</Link>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Madina Karachai</h1>
+      <div className='img__container'>
+        <img className='img-madina' src='/madina.jpeg' alt='madina'/>
+        <img className='img-madina' src='/madina_school.jpeg' alt='madina_school'/>
+      </div>
+      <a className='link-git' href="https://github.com/madishak/yandex_test">Git репозиторий</a>
+      <p>
+        Когда, после тёмных дней, ты возвращаешься за работу,
+        когда в свободное время ты интересуешься машиннным обучением, когда с радостью преподаёшь
+        WEB, и день не день, без строчки кода - ты точно на своём месте. Так я
+        понимаю, что программирование - это моё! Мне нравится, то что создается
+        людьми, структурированное, творческое и виден результат своих трудов,
+        программирование как раз из этой области, поэтому и любимое занятие))!
+      </p>
+      <Link to="game" className="btn">Play Game</Link>
+      <p>CSS Trick</p>
+      <video className="video" controls='controls' poster='/madina.jpeg'><source src='/karachai.mp4'/></video>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,9 +1,95 @@
+import { createElement } from "react";
+import { useState, useRef, useEffect } from "react";
+import "./styles.css";
+
+const handleDrag = (e) => {
+  e.dataTransfer.setData("meat", e.target.id);
+};
+
+const imgArrGenerator = () => {
+  let imgArr = [];
+
+  for (let i = 0; i <= 119; i++) {
+    let img = createElement("img", {
+      alt: "meat",
+      src: "/meat.png",
+      key: i,
+      className: "meat",
+      id: `drag${i}`,
+      draggable: "true",
+      onDragStart: handleDrag,
+    });
+    imgArr.push(img);
+  }
+  return imgArr;
+};
+
 const Game = () => {
-    return (
-        <div>
-            <h1>Крутая игра на JS!</h1>
-        </div>
-    )
+  const [countFood, setCountFood] = useState(0);
+  const [overText, setOverText] = useState("");
+  const [style, setStyle] = useState({ display: "none" });
+  const timer = useRef(null);
+  const [imgArr, setImgArr] = useState(imgArrGenerator());
+
+  useEffect(() => {
+    timer.current = setInterval(() => gameOver(), 10000);
+    return () => {
+      clearInterval(timer.current);
+    };
+  }, []);
+
+  const handleAllowDrop = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    let data = e.dataTransfer.getData("meat");
+    e.target.appendChild(document.getElementById(data));
+    setCountFood(countFood + 1);
+  };
+
+  const handleClick = () => {
+    setCountFood(0);
+    setOverText("");
+    setStyle({ display: "none" });
+    setImgArr(imgArrGenerator());
+  };
+
+  const gameOver = () => {
+    setOverText("GAME OVER");
+    setStyle({ display: "block" });
+  };
+
+  return (
+    <div>
+      <h1>Игра на JS!</h1>
+      <p className="text">
+        Давайте немного опробуем на практике возможности JS! Помоги хаски
+        собрать как можно больше еды. Нужно из левого бокса, перемещать в
+        правый. Игра на время. P.S. Эта версия игры - прототип. В дальнейшем можно доработать.
+      </p>
+      <img src="/siberian-husky.png" className="game_hero" alt="husky" />
+      <p>{countFood}</p>
+      <p>{overText}</p>
+      <input
+        type="button"
+        style={style}
+        className="restartBtn"
+        onClick={() => handleClick()}
+        value="Restart"
+      />
+      <div className="wrapper">
+        <div className="inner-left">{imgArr && imgArr.map((el) => el)}</div>
+        <div
+          className="inner-right"
+          id="div1"
+          onDrop={handleDrop}
+          onDragOver={handleAllowDrop}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Game;
